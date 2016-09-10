a6b52f987ba1561bc1643661079e48f1b1fb603b
Rodrigo Salmeron <Salmeron@Echo.local>
2016-09-09 19:20:06 -0500

    fullscreen

--- _site/js/map.js
+++ _site/js/map.js
@@ -6,26 +6,29 @@ function initMap() {
     styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}],
     scrollwheel: false
   };
+
   var mapCastellanos = new google.maps.Map(document.getElementById("map"), locCastellanosOptions);
   var iconCastellanos = {
-       url: '/img/marker.svg',
-       size: new google.maps.Size(30,120),
-       origin: new google.maps.Point(10,0),
-       anchor: new google.maps.Point(10,75)
+       url: '/img/marker3.svg',
+       size: new google.maps.Size(50,110),
+       origin: new google.maps.Point(0,0),
+       anchor: new google.maps.Point(10,90)
  };
+
   var locCastellanosMarker = new google.maps.Marker({
     position: locCastellanos,
     map: mapCastellanos,
     title: "Castellanos & Asociados",
-    // animation: google.maps.Animation.DROP,
+    animation: google.maps.Animation.DROP,
     icon: iconCastellanos
   });
-  locCastellanosMarker.addListener('click', toggleBounce);
-  function toggleBounce() {
-    if (locCastellanosMarker.getAnimation() !== null) {
-      locCastellanosMarker.setAnimation(null);
-    } else {
-      locCastellanosMarker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-}
+
+//  function toggleBounce() {
+//    if (locCastellanosMarker.getAnimation() !== null) {
+//      locCastellanosMarker.setAnimation(null);
+//    } else {
+//      locCastellanosMarker.setAnimation(google.maps.Animation.BOUNCE);
+//    }
+//  }
+//  locCastellanosMarker.addListener('click', toggleBounce);
+};
--- contacto.md
+++ contacto.md
@@ -6,27 +6,26 @@ photo: contacto
 ---
 
 <section class="cards">
-<div class="halfcard">
-<p>Teléfonos: <a href="tel:{{ site.tel1}}"> {{site.tel1}}</a> | <a href="tel:{{ site.tel2}}">{{site.tel2}}</a></p>
+  <div class="halfcard">
+    <p>Teléfonos: <a href="tel:{{ site.tel1}}"> {{site.tel1}}</a> | <a href="tel:{{ site.tel2}}">{{site.tel2}}</a></p>
+    <form action="https://formspree.io/{{ site.email }}" method="POST">
+        <label for="name">Nombre:</label>
+          <input type="text" name="name"><br>
+        <label for="name">Email:</label>
+          <input type="email" name="_replyto"><br>
+        <label for="message">Mensaje:</label>
+          <textarea type="text" name="message"></textarea><br>
+        <input class="submitbutton" type="submit" value="Enviar">
+    </form>
+  </div>
 
-<form action="https://formspree.io/{{ site.email }}" method="POST">
-    <label for="name">Nombre:</label>
-      <input type="text" name="name"><br>
-    <label for="name">Email:</label>
-      <input type="email" name="_replyto"><br>
-    <label for="message">Mensaje:</label>
-      <textarea type="text" name="message"></textarea><br>
-    <input class="submitbutton" type="submit" value="Enviar">
-</form>
-
-</div>
-<div class="halfcard">
-<div id="map"></div>
-</div>
+  <div class="halfcard">
+    <div id="map"></div>
+    <p>San Francisco 1626, Int. 804. Col. del Valle, Del. Benito Juárez.</p>
+  </div>
 </section>
 
 <script src="{{site.baseurl}}js/map.js"></script>
-
 <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLQc8Mu-fjVVgkBV77SKMaYiGJV0jaRQw&callback=initMap">
 </script>
